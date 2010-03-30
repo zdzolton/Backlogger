d41fef78c057d07b48143e361e373d3740f17c9a
Zachary Zolton <zachary.zolton@gmail.com>
2010-03-29 23:44:16 -0500

    when moving an existing story to a different sprint, assign it the highest priority

--- evently/story_dialog/showEdit/selectors/form/submit.js
+++ evently/story_dialog/showEdit/selectors/form/submit.js
@@ -3,11 +3,17 @@ function(e) {
   $('#story_dialog').dialog('close');
   var id = $("input[name=_id]", f).val();
   $.log('Updating doc ID: ' + id);
-  var db = $$(this).app.db;
+  var app = $$(this).app;
+  var db = app.db;
   db.openDoc(id, {
     success: function(doc) {
       $.log('Retrieved doc:');
       $.log(doc);
+      
+      $.log('Old sprint number:');
+      var oldSprintNum = doc.sprint_number;
+      $.log()
+      
       $.extend(doc, {
         name: $("input[name=name]", f).val(),
         sprint_number: parseInt($("input[name=sprint_number]", f).val()),
@@ -16,9 +22,36 @@ function(e) {
         acceptance_criteria: $("textarea[name=acceptance_criteria]", f).val(),
         notes: $("textarea[name=notes]", f).val()
       });
-      $.log('Updated doc:');
-      $.log(doc);
-      db.saveDoc(doc);
+      
+      if (doc.sprint_number == oldSprintNum) {
+        $.log('Updated doc:');
+        $.log(doc);
+        db.saveDoc(doc);        
+      } else {
+        $.log('Sprint number changed:');
+        $.log(doc.sprint_number);
+        
+        // TODO: merge with code from showNew.selectors.form.submit
+        app.view('backlog-stories', {
+          limit: 1,
+          descending: false,
+          reduce: false,
+          startkey: [doc.sprint_number],
+          endkey: [doc.sprint_number, {}],
+          success: function(res) {
+            $.log('Got view result for priority:');
+            $.log(res);
+            var firstRow = res.rows[0];
+            var lastPriority = firstRow ? firstRow.value.priority : 1;
+            $.log('Last priority:');
+            $.log(lastPriority);
+            doc.priority = lastPriority + 1;
+            $.log('Saving document:');
+            $.log(doc);
+            app.db.saveDoc(doc);
+          }
+        });
+      }
     }
   });
   
