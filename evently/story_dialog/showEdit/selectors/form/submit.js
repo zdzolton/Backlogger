6ad93c1f5f06a78e10153b1a5bf4a099c9d3ebda
Zachary Zolton <zachary.zolton@gmail.com>
2010-04-05 10:48:13 -0500

    removed the sprint number inputs from the edit story form

--- evently/story_dialog/showEdit/selectors/form/submit.js
+++ evently/story_dialog/showEdit/selectors/form/submit.js
@@ -1,35 +1,4 @@
 function(e) {
-  
-  function parseSprintNumber() {
-    var sprintNumber = $("input[name=sprint_number]", f).val();
-    var sprintYear = $("input[name=sprint_year]", f).val();
-    $.log('A');
-    if (sprintNumber.length > 0) {
-      if (sprintYear.length > 0) {
-        $.log('B');
-        sprintNumber = parseInt(sprintNumber);
-        sprintYear = parseInt(sprintYear);
-        // (^u^) cheap zero-pad!
-        sprintNumber = (sprintNumber < 10 ? '0' : '') + String(sprintNumber); 
-        sprintNumber = sprintYear + '-' + sprintNumber;
-      } else {
-        $.log('C');
-        alert('Sprint number without year!');
-        return;
-      }
-    } else {
-      if (sprintYear.length > 0) {
-        $.log('D');
-        alert('Sprint year without number!');
-        return;
-      } else {
-        $.log('E');
-        sprintNumber = "unassigned";
-      }
-    }
-    return sprintNumber;
-  }
-  
   var f = $(this);
   $('#story_dialog').dialog('close');
   var id = $("input[name=_id]", f).val();
@@ -40,43 +9,16 @@ function(e) {
     success: function(doc) {
       $.log('Retrieved doc:');
       $.log(doc);
-      
-      $.log('Old sprint number:');
-      var oldSprintNum = doc.sprint_number;
-      
-      $.log('New sprint number:');
-      var sprintNumber = parseSprintNumber();
-      if (typeof sprintNumber === 'undefined') {
-        $.log('Bailing early for validation problem!');
-        return;
-      }
-      $.log(sprintNumber);
-      
       $.extend(doc, {
         name: $("input[name=name]", f).val(),
-        sprint_number: sprintNumber,
         complexity: $("select[name=complexity]", f).val(),
         business_value: $("select[name=business_value]", f).val(),
         acceptance_criteria: $("textarea[name=acceptance_criteria]", f).val(),
         notes: $("textarea[name=notes]", f).val()
       });
-      
-      if (doc.sprint_number == oldSprintNum) {
-        $.log('Updated doc:');
-        $.log(doc);
-        db.saveDoc(doc);        
-      } else {
-        $.log('Sprint number changed:');
-        $.log(doc.sprint_number);
-        app.getHighestPriorityForSprint(sprintNumber, function(priority) {
-          $.log('Last priority:');
-          $.log(priority);
-          doc.priority = priority + 1;
-          $.log('Saving document:');
-          $.log(doc);
-          app.db.saveDoc(doc);
-        });
-      }
+      db.saveDoc(doc);        
+      $.log(doc);
+      $.log('Updated doc:');
     }
   });
   return false;
